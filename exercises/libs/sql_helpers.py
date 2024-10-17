f4027eb77791dba3a40cf9ffee8b17a994ea6308
thangtk2003 <thangtk2003@gmail.com>
2024-10-17 09:17:00 +0700

    Fix bug SQL test cases

--- exercises/libs/sql_helpers.py
+++ exercises/libs/sql_helpers.py
@@ -29,7 +29,7 @@ def get_mysql_connection():
         )
         return conn
     except mysql.connector.Error as err:
-        print(f"Error: {err}")
+        #print(f"Error: {err}")
         raise
 
 def execute_sql(language, code, test_cases):
@@ -37,8 +37,6 @@ def execute_sql(language, code, test_cases):
     if isinstance( dict_query_student,str):
         return dict_query_student
     test_cases = convert_list_to_dict(test_cases)
-    print(dict_query_student)
-    print(test_cases)
     message = []
     passed_tests = 0
     try:
@@ -55,35 +53,36 @@ def execute_sql(language, code, test_cases):
 
             # Execute the expected query
             expected_query = test_cases.get(query_key)
-            cursor.execute(expected_query)
-            expected_result = cursor.fetchall()
-            message_temp += f"<strong>Expected Result:</strong>" + "&nbsp;&nbsp;&nbsp;&nbsp;" +str(expected_result) + "<br>"
+            if expected_query:
+                cursor.execute(expected_query)
+                expected_result = cursor.fetchall()
+                message_temp += f"<strong>Expected Result:</strong>" + "&nbsp;&nbsp;&nbsp;&nbsp;" +str(expected_result) + "<br>"
 
-            if student_result.get('result', []) == expected_result:
-                passed_tests += 1
-                print(f'{query_key}: Pass')
-                # Thêm nội dung vào thẻ alert success khi pass
-                message.append(
-                    f'<div class="alert alert-success" role="alert">'
-                    f'{message_temp}<div style="text-align: center;"><strong>&lt;&lt;&lt;&lt;&lt;&lt; Pass &gt;&gt;&gt;&gt;&gt;&gt;</strong></div>'
-                    f'</div>'
-                )
-            else:
-                error_msg = student_result.get('error',"Please check the expected Result !!!")
-                print(f'{query_key}: Fail - Error: {error_msg}')
-                # Thêm nội dung vào thẻ alert danger khi fail
-                message.append(
-                    f'<div class="alert alert-danger" role="alert">'
-                    f'{message_temp} <div style="text-align: center;"><strong>{error_msg}</strong><br><strong>&lt;&lt;&lt;&lt;&lt;&lt; Fail &gt;&gt;&gt;&gt;&gt;&gt;</strong></div>'
-                    f'</div>'
-                )
-        header_msg = f"""
-        <div style="text-align: center;">
-        <h5> <span style="color: black;"> &lt;&lt;&lt;&lt;&lt;&lt;RUNNING TEST CASES&gt;&gt;&gt;&gt;&gt;&gt;</span></h5>
-        <h6> <span style="color: red;">You have passed {passed_tests} out of 
-        {len(test_cases)} total test cases. </span></h6></div><br>
-        
-        """
+                if student_result.get('result', []) == expected_result:
+                    passed_tests += 1
+                    #print(f'{query_key}: Pass')
+                    # Thêm nội dung vào thẻ alert success khi pass
+                    message.append(
+                        f'<div class="alert alert-success" role="alert">'
+                        f'{message_temp}<div style="text-align: center;"><strong>&lt;&lt;&lt;&lt;&lt;&lt; Pass &gt;&gt;&gt;&gt;&gt;&gt;</strong></div>'
+                        f'</div>'
+                    )
+                else:
+                    error_msg = student_result.get('error',"Please check the expected Result !!!")
+                    #print(f'{query_key}: Fail - Error: {error_msg}')
+                    # Thêm nội dung vào thẻ alert danger khi fail
+                    message.append(
+                        f'<div class="alert alert-danger" role="alert">'
+                        f'{message_temp} <div style="text-align: center;"><strong>{error_msg}</strong><br><strong>&lt;&lt;&lt;&lt;&lt;&lt; Fail &gt;&gt;&gt;&gt;&gt;&gt;</strong></div>'
+                        f'</div>'
+                    )
+            header_msg = f"""
+            <div style="text-align: center;">
+            <h5> <span style="color: black;"> &lt;&lt;&lt;&lt;&lt;&lt;RUNNING TEST CASES&gt;&gt;&gt;&gt;&gt;&gt;</span></h5>
+            <h6> <span style="color: red;">You have passed {passed_tests} out of 
+            {len(test_cases)} total test cases. </span></h6></div><br>
+            
+            """
         message.insert(0, header_msg)
     except mysql.connector.Error as e:
         return {'error': f"SQL Error: {e}"}
@@ -94,7 +93,7 @@ def execute_sql(language, code, test_cases):
         if conn is not None:  # Check if connection was created
             conn.close() 
     combined_message = ''.join(message)
-    print(combined_message)
+    #print(combined_message)
     return combined_message
 
 def split_queries(queries: str) -> dict:
@@ -138,24 +137,23 @@ def execute_student_query(cursor, query):
     try:
         cursor.execute(query)  # Execute the provided query
         student_result = cursor.fetchall()  # Get the result from the student's query
-        print("Student's Result:", student_result)
         return {'result': student_result}
     except ProgrammingError as e:
-        print(f"Programming Error executing student's query: {e}")
+        #print(f"Programming Error executing student's query: {e}")
         return {'error': f"Programming Error: {e}"}
     except DataError as e:
-        print(f"Data Error executing student's query: {e}")
+        #print(f"Data Error executing student's query: {e}")
         return {'error': f"Data Error: {e}"}
     except IntegrityError as e:
-        print(f"Integrity Error executing student's query: {e}")
+        #print(f"Integrity Error executing student's query: {e}")
         return {'error': f"Integrity Error: {e}"}
     except OperationalError as e:
-        print(f"Operational Error executing student's query: {e}")
+        #print(f"Operational Error executing student's query: {e}")
         return {'error': f"Operational Error: {e}"}
     except Error as e:
-        print(f"General SQL Error executing student's query: {e}")
+        #print(f"General SQL Error executing student's query: {e}")
         return {'error': f"General SQL Error: {e}"}
     except Exception as e:
         # This will catch any other type of exception that is not caught by the specific mysql.connector errors
-        print(f"Unexpected error: {e}")
+        #print(f"Unexpected error: {e}")
         return {'error': f"Unexpected Error: {e}"}
